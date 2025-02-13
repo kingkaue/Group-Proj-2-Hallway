2bffc05e25d7588d95160e2fe83aa20dba7011e7
kingkaue <kaueking@gmail.com>
2025-02-13 18:59:18 -0500

    Added Pause menu functionality and fixed player camera

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -10,6 +10,8 @@ public class PlayerCamera : MonoBehaviour
     float xRotation;
     float yRotation;
 
+    private GameObject gameManagerObject;
+
     // Start is called before the first frame update
     void Start()
     {
