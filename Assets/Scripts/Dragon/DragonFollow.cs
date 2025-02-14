ac9e38fc7305822a8a4a27f514c8bc28a9e4cc2c
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-02-14 17:01:20 -0500

    Dragon backs off when looked at

--- Assets/Scripts/Dragon/DragonFollow.cs
+++ Assets/Scripts/Dragon/DragonFollow.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,7 +10,13 @@ public class DragonFollow : MonoBehaviour
     private GameObject destination;   
     private NavMeshAgent agent;
 
-   
+    private float speed;
+
+    [Header("BackUp")]
+    private bool isGazingUpon;
+    public GameObject player;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +24,7 @@ public class DragonFollow : MonoBehaviour
         destination = GameObject.FindGameObjectWithTag("Player");
         // Connects to Nav Mesh Agent
         agent = GetComponent<NavMeshAgent>();
+        speed = 5;
 
     }
 
@@ -26,6 +34,29 @@ public class DragonFollow : MonoBehaviour
 
         // Updates the destionation to players current location
         agent.SetDestination(destination.transform.position);
-        
+
+        //Code for speed
+        /*
+        Vector3 direction = destination.transform.forward;
+        Vector3 movementVector = direction * speed * Time.deltaTime;
+        transform.position += movementVector;
+        */
+
+        //If in raycast veiw, move backwards
+        if (isGazingUpon)
+        {
+            transform.Translate(-destination.transform.position);
+        }
+
+    }
+
+    public void GazingUpon()
+    {
+        isGazingUpon = true;
+    }
+
+    public void NotGazingUpon()
+    {
+        isGazingUpon = false;
     }
 }
