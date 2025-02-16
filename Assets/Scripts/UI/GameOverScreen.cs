9fd60fae7ec6cde15e8443f89947b19481875465
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-02-16 12:17:37 -0500

    Timer / Nav Update
    Timer to end game in 90 sec
    Dragon now navigates properly, and is faster

--- Assets/Scripts/UI/GameOverScreen.cs
+++ Assets/Scripts/UI/GameOverScreen.cs
@@ -1,15 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
 {
     public GameObject gameOverScreen;
+    public float timeRemaining = 90;
+    public bool timerIsRunning = false;
+
+    public TextMeshProUGUI timeText;
+
+    float Timer;
 
     void Start()
     {
         gameOverScreen.SetActive(false);
+        timerIsRunning = true;
+    }
+
+    private void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                timeText.text = "";
+                GameOver();
+            }
+        }
     }
 
     public void GameOver()
@@ -20,6 +47,11 @@ public class GameOverScreen : MonoBehaviour
         //Unlocks cursor
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+
+        //Stops timer
+        timeRemaining = 0;
+        timerIsRunning = false;
+        timeText.text = "";
     }
 
     public void Restart()
@@ -30,5 +62,14 @@ public class GameOverScreen : MonoBehaviour
     public void BackToMenu()
     {
         SceneManager.LoadScene("Menu");
+    }
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeRemaining / 60);
+        float seconds = Mathf.FloorToInt(timeRemaining % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
