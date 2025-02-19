86e2c4b3cf833d1099f6961dd5e456d322477094
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-02-18 20:26:33 -0500

    Update Credits.cs

--- Assets/Scripts/UI/Credits.cs
+++ Assets/Scripts/UI/Credits.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class Credits : MonoBehaviour
 {
+    private void Start()
+    {
+        //Unlocks cursor
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
     public void BackToMenu()
     {
         SceneManager.LoadScene("Menu");
