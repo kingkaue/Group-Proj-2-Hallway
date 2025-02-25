55144302a2d1fa2f5016efd304c1b7737e3ac28d
kingkaue <kaueking@gmail.com>
2025-02-25 16:36:19 -0500

    Torch Light
    Changed light to flicker like a torch

---
+++ Assets/Scripts/Player/Torch.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Torch : MonoBehaviour
+{
+    private Light lightToFlicker;
+    [SerializeField, Range(0f, 5f)] private float minIntensity = 0.5f;
+    [SerializeField, Range(0f, 5f)] private float maxIntensity = 1.2f;
+    [SerializeField, Min(0f)] private float timeBetweenIntensity = 0.1f;
+    private float currentTimer;
+
+    private void Awake()
+    {
+        if (lightToFlicker == null)
+        {
+            lightToFlicker = GetComponent<Light>();
+        }
+
+        ValidateIntensityBounds();
+    }
+
+    private void Update()
+    {
+        currentTimer += Time.deltaTime;
+        if (!(currentTimer >= timeBetweenIntensity))
+        {
+            return;
+        }   
+        lightToFlicker.intensity = Random.Range(minIntensity, maxIntensity);
+        currentTimer = 0;
+    }
+
+    private void ValidateIntensityBounds()
+    {
+        if (!(minIntensity > maxIntensity))
+        {
+            return;
+        }
+
+        (minIntensity, maxIntensity) = (maxIntensity, minIntensity);
+    }
+}
