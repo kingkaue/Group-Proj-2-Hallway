7fc61071770b53481220983d568f86bac76cdf1c
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-03-05 11:44:11 -0500

    Dragon Animations In (with code)

--- Assets/Scripts/Dragon/DragonFollow.cs
+++ Assets/Scripts/Dragon/DragonFollow.cs
@@ -13,13 +13,20 @@ public class DragonFollow : MonoBehaviour
     [Header("BackUp")]
     private bool isGazingUpon;
 
+    [Header("Animation")]
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
+        
         // sets destination
         destination = GameObject.FindGameObjectWithTag("Player");
         // Connects to Nav Mesh Agent
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        //Initiates dragon walking animation
+        animator.SetBool("IsGazingUponTrue", false);
     }
 
     // Update is called once per frame
@@ -45,10 +52,13 @@ public class DragonFollow : MonoBehaviour
         //If in raycast veiw, move backwards
         if (isGazingUpon == true)
         {
+            
             //Reverses destination direction
             Vector3 oppositeDirection = transform.position - destination.transform.position;
-            
+
             agent.SetDestination(oppositeDirection);
+            //Initiates dragon flapping animation
+            animator.SetBool("IsGazingUponTrue", true);
             Destroy(this.gameObject, 3f);
         }
         else
